2d5f9cdbfbbcec3789662f1caefa945b2a8313ee
Helene Normant <normanthelene@gmail.com>
2023-08-02 19:05:26 +0200

    Animation hover header

--- src/components/header/header/Header.jsx
+++ src/components/header/header/Header.jsx
@@ -4,11 +4,11 @@ import { BsArrowDown, BsArrowRight } from 'react-icons/bs';
 const Header = () => {
     return (
         <div className="header-container">  
-         <div className="mt-3 border-1 border-solid border-dark"><hr /></div>
+         <div className="mt-3 border-[0.7px] border-solid border-dark"><hr /></div>
           <div className='flex justify-between' >
                 <div className='flex items-center'>
-                        <h1 className='ml-[100px] text-3xl text-dark'> <BsArrowRight/> </h1>
-                        <h1 className='header-hover-download text-3xl text-dark cursor-pointer'>DOWNLOAD CV</h1>
+                        <h1 className='ml-[73px] text-3xl text-dark'> <BsArrowRight/> </h1>
+                        <h1 className='header-hover-download text-3xl cursor-pointer'>DOWNLOAD CV</h1>
                         <h1 className='ml-[200px] text-center text-3xl text-dark'> <BsArrowDown/> </h1>
                         <h1 className='header-hover-contact text-3xl text-dark cursor-pointer'>CONTACT</h1>
              </div>
@@ -18,12 +18,12 @@ const Header = () => {
                        <h1 className='text-3xl text-light'>DEVELOPER</h1>
                 </div>
                 <div className='Header-full-name'>
-                    <h1 className='w-[130px] text-center text-3xl text-dark border-b border-r border-solid border-dark'>HELENE</h1>
-                     <h1 className='w-[160px] ml-[129px] text-center text-3xl text-dark border-t border-l border-solid border-dark'>NORMANT</h1>
+                    <h1 className='w-[130px] text-center text-3xl text-dark border-[0.5px] border-b border-r border-solid border-dark'>HELENE</h1>
+                     <h1 className='w-[160px] ml-[129px] text-center text-3xl text-dark border-[0.5px] border-t border-l border-solid border-dark'>NORMANT</h1>
                     </div>
               </div>  
             </div>
-            <div className="border-1 border-solid border-dark"><hr /></div>
+            <div className="border-[0.7px] border-solid border-dark"><hr /></div>
         </div>
 )
   };

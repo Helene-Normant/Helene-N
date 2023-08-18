ce63afc0c6d766c4765f6433b84e480905a3ea3d
Helene Normant <normanthelene@gmail.com>
2023-08-18 10:49:26 +0200

    wip project section

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,8 +8,8 @@ const data = [
     id: 1,
     image: IMG1,
     title: 'Triplink | Front-end team | Graduate project',
-    subtitle :'Social network for travel',
-    description:'Work still in porgress: In our last year of education, I am part of a 4-member group project. Our team has 2 front-end and 2 back-end members. I am the designer and front-end developer, showcasing teamwork and a key role in project organization.',
+    subtitle :'Traveler social network',
+    description:'WIP: During our final year, I am participating in a group project with 4 members. Our team consists of two people working on the front and two on the back. I have a multifaceted role, serving as both a designer and a front-end developer. Additionally, I am demonstrating strong teamwork abilities and playing a crucial role in organizing the project.',
     github: 'https://github.com/Helene-Normant/Ride_my_bike',
     demo:''
   },
@@ -18,7 +18,7 @@ const data = [
     id: 2,
     image: IMG2,
     title: 'Castorlama | Front-end team',
-    subtitle: 'Vintage Furniture shopping',
+    subtitle: 'Furnitures marketplace',
     description:'This two-week project during my studies provided me with a better understanding of the interaction between the back-end and the front-end. It also allowed us to establish a structured organization based on the agile methodology. I was able to handle both the design and front-end development aspects effectively.',
     github: 'https://github.com/Helene-Normant/pimp_emoji',
     demo:''
@@ -28,7 +28,7 @@ const data = [
     id: 3,
     image: IMG3,
     title: 'Portfolio_v1 | Personal project',
-    subtitle:'My first portfolio in react',
+    subtitle:'My first react portfolio',
     description: 'During my search for an apprenticeship, I chose to deepen my knowledge in front-end development and focus on React. This led me to combine learning with practicality by creating a portfolio. This not only allowed me to grasp the fundamentals but also showcased my skills during my job search efforts.',
     github: 'https://github.com/Helene-Normant/helene-normant.github.io',
     demo:''

10ad937a8ced8238d88ec59717a3a58956265d38
Helene Normant <normanthelene@gmail.com>
2023-08-19 18:32:34 +0200

    create data file for export

---
+++ src/components/cards/data.js
@@ -0,0 +1,37 @@
+import IMG1 from '../../assets/triplink.png'
+import IMG2 from '../../assets/Castorlama.png'
+import IMG3 from '../../assets/portfolio-v1.png'
+
+
+export const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Triplink | Front-end team | Graduate project',
+    subtitle :'Traveler social network',
+    description:'WIP: During our final year, I am participating in a group project with 4 members. Our team consists of two people working on the front and two on the back. I have a multifaceted role, serving as both a designer and a front-end developer. Additionally, I am demonstrating strong teamwork abilities and playing a crucial role in organizing the project.',
+    github: 'https://github.com/Helene-Normant/Ride_my_bike',
+    demo:''
+  },
+
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Castorlama | Front-end team',
+    subtitle: 'Furnitures marketplace',
+    description:'This two-week project during my studies provided me with a better understanding of the interaction between the back-end and the front-end. It also allowed us to establish a structured organization based on the agile methodology. I was able to handle both the design and front-end development aspects effectively.',
+    github: 'https://github.com/Helene-Normant/pimp_emoji',
+    demo:''
+  },
+
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Portfolio_v1 | Personal project',
+    subtitle:'My first react portfolio',
+    description: 'During my search for an apprenticeship, I chose to deepen my knowledge in front-end development and focus on React. This led me to combine learning with practicality by creating a portfolio. This not only allowed me to grasp the fundamentals but also showcased my skills during my job search efforts.',
+    github: 'https://github.com/Helene-Normant/helene-normant.github.io',
+    demo:''
+  }
+
+]
